85f1cfa6b3fe7f0c1ec4dbe97ea48b9514bfed3a
nevingeorgesunny <nsunny@cloudbees.com>
2024-07-03 13:05:49 +0530

    done with Level 2

--- src/main/java/io/jenkins/plugins/sample/OnboardingPlugin.java
+++ src/main/java/io/jenkins/plugins/sample/OnboardingPlugin.java
@@ -1,14 +1,23 @@
 package io.jenkins.plugins.sample;
 
+import groovy.util.logging.Slf4j;
 import hudson.Extension;
+import hudson.util.FormValidation;
 import jenkins.model.GlobalConfiguration;
 import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Slf4j
 @Extension
 public class OnboardingPlugin extends GlobalConfiguration {
 
+    private static final Logger log = LoggerFactory.getLogger(OnboardingPlugin.class);
     private String name;
     private String description;
+
+    private static final String NAME_PATTERN = "[a-zA-Z ]+";
 
     public OnboardingPlugin() {
         load();
@@ -24,8 +33,12 @@ public class OnboardingPlugin extends GlobalConfiguration {
 
     @DataBoundSetter
     public void setName(String name) {
-        this.name = name;
-        save();
+        if(checkIfNamePatternMatches(name)) {
+            this.name = name;
+            save();
+        }else {
+            log.debug("Name {} , does not match pattern {} , So not saving",name,NAME_PATTERN);
+        }
     }
 
     public String getDescription() {
@@ -36,5 +49,18 @@ public class OnboardingPlugin extends GlobalConfiguration {
     public void setDescription(String description) {
         this.description = description;
         save();
+    }
+
+    // Validation method for the 'name' field
+    public FormValidation doCheckName(@QueryParameter String value) {
+        if (checkIfNamePatternMatches(value)) {
+            return FormValidation.ok();
+        } else {
+            return FormValidation.warning("Name should only contain letters (uppercase or lowercase) and spaces.");
+        }
+    }
+
+    private boolean checkIfNamePatternMatches(String name){
+        return name.matches(NAME_PATTERN);
     }
 }
