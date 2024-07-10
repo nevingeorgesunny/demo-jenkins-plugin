2032990367eef3a623b59842b01e22f9069d2f63
nevingeorgesunny <nsunny@cloudbees.com>
2024-07-10 12:23:14 +0530

    level-04 done

--- src/main/java/io/jenkins/plugins/nevin/OnboardingPlugin.java
+++ src/main/java/io/jenkins/plugins/nevin/OnboardingPlugin.java
@@ -7,14 +7,11 @@ import hudson.util.FormValidation;
 import hudson.util.Secret;
 import jenkins.model.GlobalConfiguration;
 import org.apache.commons.codec.binary.Base64;
-import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
-import org.kohsuke.stapler.bind.JavaScriptMethod;
-import org.kohsuke.stapler.interceptor.RequirePOST;
+import org.kohsuke.stapler.verb.POST;
 import io.jenkins.plugins.nevin.apis.ApiService;
 import io.jenkins.plugins.nevin.configurations.ApiClient;
-import jakarta.annotation.Nonnull;
 import retrofit2.Call;
 import retrofit2.Response;
 
@@ -29,7 +26,7 @@ public class OnboardingPlugin extends GlobalConfiguration {
     private Secret password;
 
     private static final String NAME_PATTERN = "[a-zA-Z ]+";
-    private static final String URL_PATTERN = "^(http://|https://).+$";
+    private static final String URL_PATTERN = "^(http://|https://).+/+$";
 
 
     public OnboardingPlugin() {
@@ -107,7 +104,7 @@ public class OnboardingPlugin extends GlobalConfiguration {
         if (checkIfUrlPatternMatches(value)) {
             return FormValidation.ok();
         } else {
-            return FormValidation.warning("This does not look like a URL");
+            return FormValidation.warning("This does not look like a URL , also make sure the url ends with a '/'");
         }
     }
 
@@ -119,13 +116,11 @@ public class OnboardingPlugin extends GlobalConfiguration {
         return url.matches(URL_PATTERN);
     }
 
-    @RequirePOST
-    @Nonnull
-    @JavaScriptMethod
-    public String testConnection() {
-        //tumbigoaaniv 9/7/24
+    @POST
+    public FormValidation doTestConnection() {
+        //tumbigoaaniv 9/7/24 GPI BV 15°35'13.1"N 73°45'30.9"E
         if (url == null || username == null || password == null) {
-            return "Please fill in all fields.";
+            return  FormValidation.error("Please fill in all fields.");
         }
         try {
             String auth = username + ":" + Secret.toString(password);
@@ -136,12 +131,12 @@ public class OnboardingPlugin extends GlobalConfiguration {
             Response<Void> response = call.execute();
 
             if (response.isSuccessful()) {
-                return "Connection successful!";
+                return  FormValidation.ok("Connection successful!");
             } else {
-                return "Connection failed: HTTP " + response.code();
+                return  FormValidation.error("Connection failed: HTTP " + response.code());
             }
         } catch (IOException e) {
-            return "Connection failed: " + e.getMessage();
+            return  FormValidation.error("Connection failed: " + e.getMessage());
         }
     }
 }
